fbb5b77f682a692201eb255a576a752108acecff
ale7516 <vanessaalejandrbarreramartin@gmail.com>
2024-09-07 23:43:40 -0500

    Ejercicios Python
    8 condicionales

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,10 +1,27 @@
-def evaluar(caracter):
-    # TODO: Coloca aquí el código del ejercicio 4: Letra o número
-    return "";
+def evaluar(numero1, numero2, numero3, numero4):
+    numeros = [numero1, numero2, numero3, numero4]
+    n = len(numeros)
+    intercambio = True
+
+    while intercambio:
+        intercambio = False
+        for i in range(n - 1):
+            if numeros[i] > numeros[i + 1]:
+                numeros[i], numeros[i + 1] = numeros[i + 1], numeros[i]
+                intercambio = True
+
+    respuesta = ' '.join(map(str, numeros))
+    return respuesta
 
 if __name__ == '__main__':
-    print("Caracter:", end='')
-    caracter = input()
+    print("Número 1:", end="")
+    numero1 = int(input())
+    print("Número 2:", end="")                                                         
+    numero2 = int(input())
+    print("Número 3:", end="")
+    numero3 = int(input())
+    print("Número 4:", end="")
+    numero4 = int(input())
         
-    respuesta = evaluar(caracter)
+    respuesta = evaluar(numero1, numero2, numero3, numero4)
     print(respuesta)
--- src/ordenamiento.py
+++ src/ordenamiento.py
@@ -1,11 +1,22 @@
 def evaluar(numero1, numero2, numero3, numero4):
-    # TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-    return "";
+    numeros = [numero1, numero2, numero3, numero4]
+    n = len(numeros)
+    intercambio = True
+
+    while intercambio:
+        intercambio = False
+        for i in range(n - 1):
+            if numeros[i] > numeros[i + 1]:
+                numeros[i], numeros[i + 1] = numeros[i + 1], numeros[i]
+                intercambio = True
+
+    respuesta = ' '.join(map(str, numeros))
+    return respuesta
 
 if __name__ == '__main__':
     print("Número 1:", end="")
     numero1 = int(input())
-    print("Número 2:", end="")
+    print("Número 2:", end="")                                                        
     numero2 = int(input())
     print("Número 3:", end="")
     numero3 = int(input())
